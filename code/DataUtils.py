c8605a85fcc0e78ac4ba87111f1df6824d5c05fd
N. Lakmal Deshapriya <68627673+lakmalnd@users.noreply.github.com>
2021-11-12 11:51:18 +0700

    Add files via upload

---
+++ code/DataUtils.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+def __getCircle(radius):
+    x = np.arange(-radius, radius)
+    y = np.arange(-radius, radius)
+    xx, yy = np.meshgrid(x, y)
+
+    return np.sqrt(xx**2 + yy**2) < radius
+
+def __getGaussian(radius):
+    x = np.linspace(-radius, +radius, radius*2)
+    y = np.linspace(-radius, +radius, radius*2)
+    xx, yy = np.meshgrid(x, y)
+
+    d = np.sqrt(xx**2+yy**2)
+    sigma, mu = radius/2, 0.0
+    gauss = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
+    gauss = ( gauss - np.min(gauss) ) / ( np.max(gauss) - np.min(gauss) ) # scalling between 0 to 1
+
+    return gauss
+
+def centroids2Images(point_list, im_num_row, im_num_col, g_radius=20):
+
+    circle_mat = __getGaussian(g_radius)
+
+    temp_im = np.zeros((im_num_row+g_radius*2, im_num_col+g_radius*2))
+
+    for one_pnt in point_list:
+        pnt_row = int(one_pnt[0])
+        pnt_col = int(one_pnt[1])
+
+        current_patch = temp_im[g_radius+pnt_row-g_radius:g_radius+pnt_row+g_radius, g_radius+pnt_col-g_radius:g_radius+pnt_col+g_radius]
+        temp_im[g_radius+pnt_row-g_radius:g_radius+pnt_row+g_radius, g_radius+pnt_col-g_radius:g_radius+pnt_col+g_radius] = np.maximum(current_patch, circle_mat)
+
+    temp_im = temp_im[g_radius:-g_radius, g_radius:-g_radius]
+
+    return temp_im
+
+def centroids2ImagesWithoutGaussian(point_list, im_num_row, im_num_col):
+
+    temp_im = np.zeros((im_num_row, im_num_col))
+
+    for one_pnt in point_list:
+        pnt_row = int(one_pnt[0])
+        pnt_col = int(one_pnt[1])
+
+        temp_im[pnt_row-1,pnt_col-1] = 1
+
+    return temp_im
